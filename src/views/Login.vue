534b4d874442e2bed21cd5bd018ab075849dd3b8
Alexjp <alnerdo@hotmail.com>
2019-09-13 19:03:52 +1000

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+
+<template>
+  <div class="login">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LoginForm/>
+  </div>
+</template>
+<script>
+
+import LoginForm from "@/components/LoginForm.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+
+  name: "login",
+  components:{ 
+     LoginForm,
+     HelloWorld,
+  
+  }
+/*
+  data:
+  {
+    message:'hey'
+  },
+  methods:
+  {
+    addPost: function() {
+      axios
+        .post(
+          "https://markit.mijdas.com/api/requests/user/",
+          this.username,
+          this.password,
+        )
+        .then(response =>
+        {
+          console.log(response);
+        
+        })
+    },
+
+    test:function()
+    {
+      message = "h3ey"
+      
+    }
+  }
+  */
+  /*** This is an example of a native fetch request AND an additonal way of inline templating***/
+
+  /*
+  mounted() {
+    fetch("https://markit.mijdas.com/api/requests/user/")
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      });
+  },
+  template: `
+        <div>
+            <li v-for="user in users"> {{user}} </li>
+        </div>
+    `
+    */
+};
+</script>
+
+<style scoped>
+</style>
