69719a4fac8d31321054f5f2af633fb012ef4da1
iliad11 <49063431+iliad11@users.noreply.github.com>
2019-09-22 12:32:35 +1000

    Home page updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,67 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div>
+    </div>
+    <v-divider dark="true"/>
+    <div id="carouselContainer">
+   <v-carousel continuous cycle="true" height="350">
+    <v-carousel-item>
+      <v-img src="@/assets/logo.png">
+      </v-img>
+    
+    </v-carousel-item>
+    <v-carousel-item>
+      <v-img src="@/assets/Markit 1.2.png">
+      </v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img src="@/assets/markit-logo.png">
+      </v-img>
+       </v-carousel-item>
 
+      <!-- <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">Slide {{ i + 1 }}</div>
+           -->
+        <!-- </v-row>
+      </v-sheet> -->
+  </v-carousel>
+   <v-header class="display-4" :elevation="n-10">
+        <p class="font-weight-black">Welcome to MarkIt, a versatile marketing tool developed by MIJDAS</p>
+      </v-header>
+    </div>
+</div>
 </template>
 
 <script>
-export default {
-  components: {
-  },
-};
+  export default {
+    data () {
+      return {
+        // colors: [
+        //   'primary',
+        //   'secondary',
+        //   'yellow darken-2',
+        //   'red',
+        //   'orange',
+        // ],
+        
+      }
+    },
+  }
 </script>
+<style scoped>
+  #carouselContainer{
+    padding-right: 10vw;
+    padding-left: 10vw;
+    /* padding-top: 10vw; */
+    padding-bottom: 40vw;
+  }
+</style>
