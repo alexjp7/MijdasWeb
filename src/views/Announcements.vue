ee33271afa0529de4df1b53bf59d624e67a0d48b
iliad11 <49063431+iliad11@users.noreply.github.com>
2019-09-23 01:08:57 +1000

    Job board and announcements
    Set up a basic job board and annouincements pages with some dummy data

--- src/views/Announcements.vue
+++ src/views/Announcements.vue
@@ -1,14 +1,49 @@
 <template>
-    <h1></h1>
+  <div class="announcements">
+    <h1>Announcements</h1>
+    <v-card max-width="600" max-height="50" flat="true" class="mx-auto">
+      <v-container class="pa-2" fluid>
+        <v-row>
+          <v-col>
+            <v-card color="#2196F3" dark height="80">
+              <v-card-text class="white--text">
+                <div class="headline mb-2">Lab 1 Announcement</div>Lab 1 submission will be pushed back a week.
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
+        <v-row>
+          <v-col>
+            <v-card color="#2196F3" dark height="80">
+              <v-card-text class="white--text">
+                <div class="headline mb-1">Assignment 2 Announcement</div>Criteria for assignment 2 has changed
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
+/* #announcements{
+  margin-left:10%;
+  margin-right:10%;
+}
+.announcements-content {
+  padding:2%;
+}
 
+.expansionHeader {
+  padding:1%;
+}
+.v-expansion-panel-content{
+  padding:1%;
+} */
 </style>
