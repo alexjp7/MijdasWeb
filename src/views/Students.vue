60b39abea3c37e6f63a9c5e96fa8a43b1bbab727
Alexjp <alnerdo@hotmail.com>
2019-09-22 19:00:52 +1000

    Combined StudentViewer and StudentLoader, added toolbar to Student Table

---
+++ src/views/Students.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <StudentViewer/>
+    </div>
+</template>
+
+<script>
+import StudentLoader from "@/components/StudentLoader.vue";
+import StudentViewer from "@/components/StudentViewer.vue";
+export default {
+  components: {
+    StudentLoader,
+    StudentViewer,
+  }
+}
+</script>
+
+<style>
+
+</style>
