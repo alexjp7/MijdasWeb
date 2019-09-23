b2d182ae93e6fe4122d7dc7f78cb3d7e5ed7aa7d
iliad11 <49063431+iliad11@users.noreply.github.com>
2019-09-23 18:56:16 +1000

    Announcements and settings
    Added a button to create announcements and a toggle for dark mode in settings

--- src/views/Announcements.vue
+++ src/views/Announcements.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="announcements">
     <h1>Announcements</h1>
+    
     <v-card max-width="600" max-height="50" flat="true" class="mx-auto">
       <v-container class="pa-2" fluid>
         <v-row>
           <v-col>
             <v-card color="#2196F3" dark height="80">
               <v-card-text class="white--text">
-                <div class="headline mb-2">Lab 1 Announcement</div>Lab 1 submission will be pushed back a week.
+                <div class="headline mb-2">CSIT315 - Lab 1 Announcement</div>Lab 1 submission will be pushed back a week.
               </v-card-text>
             </v-card>
           </v-col>
@@ -17,18 +18,43 @@
           <v-col>
             <v-card color="#2196F3" dark height="80">
               <v-card-text class="white--text">
-                <div class="headline mb-1">Assignment 2 Announcement</div>Criteria for assignment 2 has changed
+                <div class="headline mb-1">CSIT219 - Assignment 2 Announcement</div>Criteria for assignment 2 has changed
               </v-card-text>
             </v-card>
           </v-col>
         </v-row>
+        <v-btn v-on="on" color="secondary"
+        @click="dialog = !dialog">
+                  <v-icon>mdi-plus</v-icon>
+                </v-btn>
       </v-container>
+      <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-card-text>
+              <v-text-field label="Announcement name"></v-text-field>
+              <v-textarea label="Announcement content"></v-textarea>
+            </v-card-text>
+            <v-card-actions>
+              <div class="flex-grow-1"></div>
+              <v-btn text color="primary" @click="dialog = false">Submit</v-btn>
+              <div class="field input">"@dialog"</div>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
     </v-card>
   </div>
+  
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+      return {
+        dialog: false,
+        
+      }
+  }
+};
 </script>
 
 <style scoped>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,22 @@
 <template>
-    <h1>Settings</h1>
-
+    <div class="settings">
+        <h1>Settings</h1>
+    <v-switch
+    v-model="switch1"
+      :label="`Dark Mode: ${switch1.toString()}`"
+      dark="true"
+    ></v-switch>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data () {
+      return {
+        switch1: false,
+        
+      }
+    },
 }
 </script>
 
